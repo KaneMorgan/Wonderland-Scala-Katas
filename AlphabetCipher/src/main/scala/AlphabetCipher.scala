09c51a08bd1fbb2cc146d82423b965f2d7ccd230
Kane Morgan <kane.a.s.morgan@gmail.com>
2016-04-28 00:46:10 +0100

    solutions for alphabet cipher

--- AlphabetCipher/src/main/scala/AlphabetCipher.scala
+++ AlphabetCipher/src/main/scala/AlphabetCipher.scala
@@ -1,14 +1,69 @@
 object AlphabetCipher {
+
+  val alphabet = ('a' to 'z').mkString("")
+
   def encode(keyword: String, message: String): String = {
-    ??? //keyword
+    val ciphers = repeatKeyWord(keyword,message)
+    message.zip(ciphers).map{case (x:Char,y:Char) => encodeCharacter(x,y)}.mkString("")
   }
+  
+  def encodeCharacter(cipher:Char,raw:Char):String =
+  encodeCharacter(cipher.toString,raw.toString)
+
+  def encodeCharacter(cipher:String, raw:String):String =
+  offsetAlphabet(cipher).charAt(alphabet.indexOf(raw)).toString()
 
   def decode(keyword: String, message: String): String = {
-    ??? //keyword
+    val ciphers = repeatKeyWord(keyword,message)
+    message.zip(ciphers).map{case (x:Char,y:Char) => decodeCharacter(x.toString, y.toString)}.mkString("")
   }
 
+  def decodeCharacter(cipher:String, raw:String):String ={
+    val offset = offsetAlphabet(raw)
+    alphabet.charAt(offset.indexOf(cipher)).toString
+  }
   def decipher(cipher: String, message: String): String = {
-    ??? //cipher
+    val repeatedCipher = cipher.zip(message).map{case (encoded, original) =>toCipher(encoded, original)}.mkString("")
+    longestRepetition(repeatedCipher, 2)
+  }
+
+  def longestRepetition(repeatedCipher: String, subStringLength:Int):String = {
+    val oldSub = repeatedCipher.substring(0,subStringLength-1)
+    val previousIndex = repeatedCipher.lastIndexOf(oldSub)
+    val subString = repeatedCipher.substring(0,subStringLength)
+    val lastIndex = repeatedCipher.lastIndexOf(subString)
+
+    if(previousIndex > lastIndex )
+      repeatedCipher.substring(lastIndex,previousIndex)
+    else
+      longestRepetition(repeatedCipher, subStringLength + 1)
+
+  }
+
+  def longestSequence(chars:String,fullString:String):String = {
+    if(fullString.lastIndexOf(chars) == 0)
+      fullString
+    else
+      longestSequence(fullString.substring(0,fullString.lastIndexOf(chars)),fullString)
+  }
+  def toCipher(encoded: Char, original: Char): Char = {
+    val indexEncoded = alphabet.indexOf(encoded)
+    val indexOriginal = alphabet.indexOf(original)
+    if(indexEncoded >= indexOriginal)
+      alphabet.charAt(indexEncoded-indexOriginal)
+    else
+     alphabet.charAt(26 + (indexEncoded - indexOriginal))
+  }
+
+  def repeatKeyWord(keyword:String, message:String):String =
+    if(keyword.length() > message.length()) keyword.substring(0, message.length())
+    else repeatKeyWord(keyword + keyword, message)
+
+  def offsetAlphabet(cipher:String):String = {
+    val index = alphabet.indexOf(cipher)
+    val start = alphabet.substring(0, index ).mkString
+    val tail = alphabet.substring(index).mkString
+    (tail + start)
   }
 
 }
--- AlphabetCipher/src/test/scala/AlphabetCipherSpec.scala
+++ AlphabetCipher/src/test/scala/AlphabetCipherSpec.scala
@@ -1,7 +1,26 @@
 import AlphabetCipher._
-import org.scalatest._
+import org.scalatest
+import org.scalatest.{FlatSpec, Matchers}
 
 class AlphabetCipherSpec extends FlatSpec with Matchers {
+
+  "repeatKeyWord" should "return the keyword repeated to match the method length" in {
+    assert(repeatKeyWord("four","one") == "fou")
+    assert(repeatKeyWord("one","four") == "oneo")
+  }
+
+  "offsetAlphabet" should "return the correct characters" in {
+    assert(offsetAlphabet("b") == "bcdefghijklmnopqrstuvwxyza")
+  }
+
+  "encodeChar" should "encode characters with the cipher" in {
+    assert(encodeCharacter("v","m") == "h")
+  }
+
+  "decodeChar" should "decode characters with the cipher" in {
+    assert(decodeCharacter("v","h") == "m")
+  }
+
   "encode" should "encode given a secret keyword" in {
     assert(encode("vigilance","meetmeontuesdayeveningatseven") == "hmkbxebpxpmyllyrxiiqtoltfgzzv")
     assert(encode("scones","meetmebythetree") == "egsgqwtahuiljgs")
@@ -11,6 +30,10 @@ class AlphabetCipherSpec extends FlatSpec with Matchers {
     assert(decode("vigilance","hmkbxebpxpmyllyrxiiqtoltfgzzv") == "meetmeontuesdayeveningatseven")
     assert(decode("scones","egsgqwtahuiljgs") == "meetmebythetree")
 
+  }
+
+  "toCipher" should "convert characters to ciphers" in {
+    assert(toCipher('o','t') == 'v')
   }
 
   "decipher" should "extract the secret keyword given an encrypted message and the original message" in {
